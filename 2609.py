6288d3e1214cbfc95fed3dfc0d1a50bb2e1863a6
golapaduck <golapaduck29@naver.com>
2024-04-12 03:31:09 +0900

    refactor: 백준허브 연동으로 파일 정리

--- 2609.py
+++
@@ -1,6 +0,0 @@
-a, b = map(int,input().split())
-x,y = a,b
-while(y):
-    x,y=y,x%y
-print(x)
-print((a*b)//x)
--- 2920.py
+++
@@ -1,8 +0,0 @@
-tone = list(map(int, input().split()))
-sort = sorted(tone)
-if (sort == tone):
-    print("ascending")
-elif (list(reversed(sort)) == tone):
-    print("descending")
-else:
-    print("mixed")
