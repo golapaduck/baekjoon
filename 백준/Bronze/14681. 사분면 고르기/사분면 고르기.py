aa1e52dbce036babcaa8860b98ba5cc2061c66de
golapaduck <73866752+golapaduck@users.noreply.github.com>
2024-04-12 03:30:04 +0900

    [Bronze V] Title: 사분면 고르기, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,15 @@
+a=input()
+b=input()
+a=int(a)
+b=int(b)
+if a>0:
+    if b>0:
+        print(1)
+    else :
+        print(4)
+    
+else :
+    if b>0:
+        print(2)
+    else :
+        print(3)
